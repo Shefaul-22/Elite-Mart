c4fb279062960ac4802845ab5313bf09f1afbd36
Shefaul-22 <cse1805022brur@gmail.com>
2026-03-22 14:33:42 +0600

    fix: added suspense for search params and manage products layout

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,10 +1,13 @@
 import LoginForm from "@/components/auth/LoginForm";
-import React from "react";
+import React, { Suspense } from "react"; 
 
 const LoginPage = () => {
     return (
         <div className="mt-28">
-            <LoginForm></LoginForm>
+            {/* Suspense Boundary  */}
+            <Suspense fallback={<div className="text-center p-10 font-black uppercase tracking-widest animate-pulse">Loading Login...</div>}>
+                <LoginForm />
+            </Suspense>
         </div>
     );
 };
--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -1,11 +1,13 @@
-
 import RegisterForm from "@/components/auth/RegisterForm";
-import React from "react";
+import React, { Suspense } from "react"; 
 
 const RegisterPage = () => {
   return (
     <div className="mt-28">
-      <RegisterForm></RegisterForm>
+      {/* Suspense Boundary */}
+      <Suspense fallback={<div className="text-center p-10 font-black uppercase tracking-widest animate-pulse">Loading Register...</div>}>
+        <RegisterForm />
+      </Suspense>
     </div>
   );
 };
